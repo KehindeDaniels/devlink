a1de28389102c5e60b6164b32e7fa9ff4545ef6f
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-23 05:19:47 +0100

    feat: add comprehensive color guide to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from "tailwindcss";
+// tailwind.config.ts
+import { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        purple: {
+          DEFAULT: "#633CFF", // Primary Purple
+          hover: "#BEADFF", // Purple (Hover Color)
+          light: "#EFE8FF", // Light Purple
+        },
+        gray: {
+          dark: "#333333", // Dark Grey
+          DEFAULT: "#737373", // Grey
+          light: "#D9D9D9", // Light Grey
+          border: "#FAFAFA", // Border color
+        },
+        white: "#FFFFFF", // White
+        red: "#FF3939", // Red
       },
     },
   },
   plugins: [],
 };
+
 export default config;

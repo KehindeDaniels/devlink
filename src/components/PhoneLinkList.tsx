4ffbd596c51576d178dec007db836b5f06f7b627
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-25 09:36:27 +0100

    added scroll to links

--- src/components/PhoneLinkList.tsx
+++ src/components/PhoneLinkList.tsx
@@ -1,11 +1,10 @@
-// src/components/PhoneLinkList.tsx
 import React from "react";
 
 const PhoneLinkList: React.FC<{
   links: { id: string; platform: string; url: string }[];
 }> = ({ links }) => {
   return (
-    <div className="flex flex-col space-y-4 w-1/2 mx-auto">
+    <div className="flex flex-col space-y-4 w-1/2 mx-auto h-1/2 overflow-auto">
       {links.map((link) => (
         <div key={link.id} className="bg-gray-200 rounded-md p-2">
           <div className="text-sm font-medium">{link.platform}</div>
